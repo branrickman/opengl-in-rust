d3b48dd59ba3f02b2c03863b2e7b242fbaac7331
Bryan Brickman <bryanlbrick@gmail.com>
2021-10-02 16:45:18 -0500

    Initial multicolor triangle commit

--- src/render_gl.rs
+++ src/render_gl.rs
@@ -1,110 +1,109 @@
-pub mod render_gl {
-    use gl;
-    use std;
-    use std::ffi::{CString, CStr};
+use gl;
+use std;
+use std::ffi::{CString, CStr};
 
-    pub struct Program {
-        id: gl::types::GLuint,
-    }
+pub struct Program {
+    id: gl::types::GLuint,
+}
 
-    impl Program {
-        pub fn from_shaders(shaders: &[Shader]) -> Result<Program, String> {
-            let program_id = unsafe { gl::CreateProgram() };
+impl Program {
+    pub fn from_shaders(shaders: &[Shader]) -> Result<Program, String> {
+        let program_id = unsafe { gl::CreateProgram() };
 
-            for shader in shaders {
-                unsafe { gl::AttachShader(program_id, shader.id()); }
-            }
-
-            unsafe { gl::LinkProgram(program_id); }
-
-            //error handling here
-            let mut success: gl::types::GLint = 1;
-            unsafe {
-                gl::GetProgramiv(program_id, gl::LINK_STATUS, &mut success);
-            }
-
-            if success == 0 {
-                let mut len: gl::types::GLint = 0;
-                unsafe {
-                    gl::GetProgramiv(program_id, gl::INFO_LOG_LENGTH, &mut len);
-                }
-
-                let error = create_whitespace_cstring_with_len(len as usize);
-
-                unsafe {
-                    gl::GetProgramInfoLog(
-                        program_id,
-                        len,
-                        std::ptr::null_mut(),
-                        error.as_ptr() as *mut gl::types::GLchar
-                    );
-                }
-                return Err(error.to_string_lossy().into_owned());
-            }
-
-            for shader in shaders {
-                unsafe {gl::DetachShader(program_id, shader.id()); }
-            }
-
-            Ok(Program { id: program_id })
+        for shader in shaders {
+            unsafe { gl::AttachShader(program_id, shader.id()); }
         }
 
-        pub fn id(&self) -> gl::types::GLuint {
-            self.id
-        }
+        unsafe { gl::LinkProgram(program_id); }
 
-        pub fn set_used(&self) { //"use" is a reserved keyword, otherwise this would be use()
-            unsafe {
-                gl::UseProgram(self.id);
-            }
-        }
-    }
-
-impl Drop for Program {
-    fn drop(&mut self) {
+        //error handling here
+        let mut success: gl::types::GLint = 1;
         unsafe {
-            gl::DeleteProgram(self.id);
+            gl::GetProgramiv(program_id, gl::LINK_STATUS, &mut success);
+        }
+
+        if success == 0 {
+            let mut len: gl::types::GLint = 0;
+            unsafe {
+                gl::GetProgramiv(program_id, gl::INFO_LOG_LENGTH, &mut len);
+            }
+
+            let error = create_whitespace_cstring_with_len(len as usize);
+
+            unsafe {
+                gl::GetProgramInfoLog(
+                    program_id,
+                    len,
+                    std::ptr::null_mut(),
+                    error.as_ptr() as *mut gl::types::GLchar
+                );
+            }
+            return Err(error.to_string_lossy().into_owned());
+        }
+
+        for shader in shaders {
+            unsafe {gl::DetachShader(program_id, shader.id()); }
+        }
+
+        Ok(Program { id: program_id })
+    }
+
+    pub fn id(&self) -> gl::types::GLuint {
+        self.id
+    }
+
+    pub fn set_used(&self) { //"use" is a reserved keyword, otherwise this would be use()
+        unsafe {
+            gl::UseProgram(self.id);
         }
     }
 }
 
-    pub struct Shader {
-        id: gl::types::GLuint,
+impl Drop for Program {
+fn drop(&mut self) {
+    unsafe {
+        gl::DeleteProgram(self.id);
     }
+}
+}
 
-    impl Shader {
-        pub fn from_source(
-            source: &CStr,
-            kind: gl::types::GLenum
-        ) -> Result<Shader, String> {
-            let id = shader_from_source(source, kind)?;
-            Ok(Shader { id })  
-        }
+pub struct Shader {
+    id: gl::types::GLuint,
+}
 
-        pub fn id(&self) -> gl::types::GLuint { //gives the shader id
-            self.id
-        }
-
-        pub fn from_vert_source(source: &CStr) -> Result<Shader, String> {
-            Shader::from_source(source, gl::VERTEX_SHADER)
-        }
-
-        pub fn from_frag_source(source: &CStr) -> Result<Shader, String> {
-            Shader::from_source(source, gl::FRAGMENT_SHADER)
-        }
-    }
-
-    impl Drop for Shader {
-        fn drop(&mut self) {
-            unsafe {
-                gl::DeleteShader(self.id);
-            }
-        }
-    }
-
-    fn shader_from_source(
+impl Shader {
+    pub fn from_source(
         source: &CStr,
         kind: gl::types::GLenum
+    ) -> Result<Shader, String> {
+        let id = shader_from_source(source, kind)?;
+        Ok(Shader { id })  
+    }
+
+    pub fn id(&self) -> gl::types::GLuint { //gives the shader id
+        self.id
+    }
+
+    pub fn from_vert_source(source: &CStr) -> Result<Shader, String> {
+        Shader::from_source(source, gl::VERTEX_SHADER)
+    }
+
+    pub fn from_frag_source(source: &CStr) -> Result<Shader, String> {
+        Shader::from_source(source, gl::FRAGMENT_SHADER)
+    }
+}
+
+impl Drop for Shader {
+    fn drop(&mut self) {
+        unsafe {
+            gl::DeleteShader(self.id);
+        }
+    }
+}
+
+fn shader_from_source(
+    source: &CStr,
+    kind: gl::types::GLenum
     ) -> Result<gl::types::GLuint, String> {
         let id = unsafe { gl::CreateShader(kind) };
         unsafe {
@@ -112,40 +111,39 @@ impl Drop for Program {
             gl::CompileShader(id);
         }
 
-        let mut success: gl::types::GLint = 1;
+    let mut success: gl::types::GLint = 1;
+    unsafe {
+        gl::GetShaderiv(id, gl::COMPILE_STATUS, &mut success);
+    }
+
+    if success == 0 {
+        let mut len: gl::types::GLint = 0;
         unsafe {
-            gl::GetShaderiv(id, gl::COMPILE_STATUS, &mut success);
+            gl::GetShaderiv(id, gl::INFO_LOG_LENGTH, &mut len);
         }
 
-        if success == 0 {
-            let mut len: gl::types::GLint = 0;
-            unsafe {
-                gl::GetShaderiv(id, gl::INFO_LOG_LENGTH, &mut len);
-            }
+        let error = create_whitespace_cstring_with_len(len as usize);
 
-            let error = create_whitespace_cstring_with_len(len as usize);
-
-            unsafe {
-                gl::GetShaderInfoLog(
-                    id,
-                    len,
-                    std::ptr::null_mut(),
-                    error.as_ptr() as *mut gl::types::GLchar
-                );
-            }
-
-            return Err(error.to_string_lossy().into_owned());
+        unsafe {
+            gl::GetShaderInfoLog(
+                id,
+                len,
+                std::ptr::null_mut(),
+                error.as_ptr() as *mut gl::types::GLchar
+            );
         }
 
-        Ok(id)
+        return Err(error.to_string_lossy().into_owned());
     }
 
-    fn create_whitespace_cstring_with_len(len: usize) -> CString {
-        // allocate buffer of correct size
-        let mut buffer: Vec<u8> = Vec::with_capacity(len + 1);
-        // fill it with len spaces
-        buffer.extend([b' '].iter().cycle().take(len));
-        // convert buffer to CString
-        unsafe { CString::from_vec_unchecked(buffer) }
-    }
+    Ok(id)
+}
+
+fn create_whitespace_cstring_with_len(len: usize) -> CString {
+    // allocate buffer of correct size
+    let mut buffer: Vec<u8> = Vec::with_capacity(len + 1);
+    // fill it with len spaces
+    buffer.extend([b' '].iter().cycle().take(len));
+    // convert buffer to CString
+    unsafe { CString::from_vec_unchecked(buffer) }
 }
--- src/triangle.frag
+++ src/triangle.frag
@@ -1,8 +1,8 @@
 #version 330 core
 
-layout (location = 0) in vec3 Position;
+out vec4 Color;
 
 void main()
 {
-    gl_Position = vec4(Position, 1.0)
+    Color = vec4(1.0f, 0.7f, 0.2f, 1.0f);
 }
